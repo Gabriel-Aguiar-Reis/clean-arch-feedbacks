1e1650024442624c3913a4f704c8050dbaa05b63
Gabriel Aguiar <lugafeagre@gmail.com>
2025-04-08 22:57:16 -0300

    fix(configs): :boom: change deprecated ormconfig.js to database.config.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core'
-import type { NestExpressApplication } from '@nestjs/platform-express'
 import { AppModule } from './app.module'
+import { ConfigService } from '@nestjs/config'
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule)
-  await app.listen(process.env.PORT || 3000)
+  const app = await NestFactory.create(AppModule)
+
+  const config = app.get(ConfigService)
+  const port = config.get<number>('PORT') ?? 3000
+
+  await app.listen(port)
+  console.log(`App running on http://localhost:${port}`)
 }
 bootstrap()
