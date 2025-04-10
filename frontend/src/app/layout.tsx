aacc8fea3fb3f1c1555de49604b2381cf185dbc1
Gabriel Aguiar <lugafeagre@gmail.com>
2025-04-10 14:46:45 -0300

    feat(configs): :sparkles: change initial next.js structure

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,10 +1,13 @@
+import '@/styles/globals.css'
+
 export default function RootLayout({
   children
-}: {
+}: Readonly<{
   children: React.ReactNode
-}) {
+}>) {
   return (
     <html lang="en">
+      <head />
       <body>{children}</body>
     </html>
   )
